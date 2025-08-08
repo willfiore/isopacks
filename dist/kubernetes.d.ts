2d6972c2fe2fe98f273b0134e287fd9f29804af4
Will Fiore <will@willfiore.com>
2025-08-08 19:13:42 +0100

    Commit dist folder

---
+++ dist/kubernetes.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    id: string;
+    name: string;
+    icons: {
+        id: string;
+        name: string;
+        url: string;
+        isIsometric: boolean;
+    }[];
+};
+export default _default;
---
+++ dist/utils/index.d.ts
@@ -0,0 +1,3 @@
+import type { FlattenedIcon, ProcessedCollection } from '../types';
+export declare const flattenCollection: (collection: ProcessedCollection) => FlattenedIcon[];
+export declare const flattenCollections: (collections: ProcessedCollection[]) => FlattenedIcon[];
